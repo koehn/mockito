d14e01d732eb03d32bd263201442b0639c581c10
Brad Koehn <brad.koehn@thomsonreuters.com>
2013-03-13 14:39:13 -0500

    Added InvocationPhase to Invocation so we can figure that out. AnswerInterceptor no longer needed.

--- src/org/mockito/internal/handler/MockHandlerImpl.java
+++ src/org/mockito/internal/handler/MockHandlerImpl.java
@@ -14,6 +14,7 @@ import org.mockito.internal.stubbing.*;
 import org.mockito.internal.verification.MockAwareVerificationMode;
 import org.mockito.internal.verification.VerificationDataImpl;
 import org.mockito.invocation.Invocation;
+import org.mockito.invocation.InvocationPhase;
 import org.mockito.mock.MockCreationSettings;
 import org.mockito.stubbing.Answer;
 import org.mockito.stubbing.AnswerInterceptor;
@@ -48,6 +49,7 @@ class MockHandlerImpl<T> implements InternalMockHandler<T> {
     public Object handle(Invocation invocation) throws Throwable {
 
 		if (invocationContainerImpl.hasAnswersForStubbing()) {
+            invocation.setPhase(InvocationPhase.DEFINE);
             // stubbing voids with stubVoid() or doAnswer() style
             InvocationMatcher invocationMatcher = matchersBinder.bindMatchers(
                     mockingProgress.getArgumentMatcherStorage(),
@@ -67,6 +69,7 @@ class MockHandlerImpl<T> implements InternalMockHandler<T> {
 
         // if verificationMode is not null then someone is doing verify()
         if (verificationMode != null) {
+            invocation.setPhase(InvocationPhase.VERIFY);
             // We need to check if verification was started on the correct mock
             // - see VerifyingWithAnExtraCallToADifferentMockTest (bug 138)
             if (((MockAwareVerificationMode) verificationMode).getMock() == invocation.getMock()) {
@@ -89,6 +92,7 @@ class MockHandlerImpl<T> implements InternalMockHandler<T> {
         StubbedInvocationMatcher stubbedInvocation = invocationContainerImpl.findAnswerFor(invocation);
 
         if (stubbedInvocation != null) {
+            invocation.setPhase(InvocationPhase.EXECUTE);
             stubbedInvocation.captureArgumentsFrom(invocation);
             AnswerInterceptor answerInterceptor = mockSettings.getAnswerInterceptor();
             if (answerInterceptor != null) {
